3e625a73d6a2359a89373686a6b426682252c767
lebrondavid <114090659+lebrondavid@users.noreply.github.com>
2024-06-07 09:38:57 -0300

    07/06

---
+++ script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = ["Pergunta 1","Pergunta 2", "Pergunta 3"]
