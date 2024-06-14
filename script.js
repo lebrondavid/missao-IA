cce2d2bf108e88ac8a870e212e3f359eb19e2766
lebrondavid <114090659+lebrondavid@users.noreply.github.com>
2024-06-14 09:47:02 -0300

    13/06

--- script.js
+++ script.js
@@ -5,8 +5,41 @@ const caixaResultados = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-   enunciado: "Pergunta 1",
-    alternativas: [
-        "Alternativa"
-    ]
-    "Pergunta 2", "Pergunta 3"]
+    {
+        enunciado: "Neymar (1992) é um futebolista brasileiro, considerado um dos maiores da história. Ganhou diversos títulos jogando pelo Santos Futebol Clube, onde iniciou sua carreira aos 11 anos de idade. Sua posição é atacante, atuando como ponta-esquerda. Neymar jogou em diversos times.",
+        alternativas: [
+            "Isso é maravilhoso!",
+            "Isso é assustador!"
+        ]
+    },
+    {
+        enunciado: "Neymar é brasileiro ?",
+        alternativas: [
+            "Sim.",
+            "Não."
+        ]
+    },
+    {
+        enunciado: "Você considera o Neymar um dos maiore sda história ?",
+        alternativas: [
+            "Sim.",
+            "Não"
+        ]
+    },
+    {
+        enunciado: "Quantos títulos o Neymar ganhu pelo Santos Futebol Clube ?",
+        alternativas: [
+            "6",
+            "3"
+        ]
+    },
+    {
+        enunciado: "Com quantos anos Neymar começou sua carreira pelo santos ?",
+        alternativas: [
+            "11 anos de idade",
+            "16 anos de idade"
+        ]
+    },
+];
+
+let atual = 0;
